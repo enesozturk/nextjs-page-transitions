55970c83168e75c1034ed41e3c97d1a926dbbe0e
enes <enesozturk.d@gmail.com>
2019-04-11 11:37:34 +0300

    use basic nav for better mobile ux

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Container, Row, Nav } from 'react-bootstrap';
 
 export default class NavBar extends React.PureComponent {
 	render() {
 		return (
-			<Navbar expand="lg" bg="dark" variant="dark">
-				<Navbar.Brand href="/">Next Js Page Transitions</Navbar.Brand>
-				<Navbar.Toggle />
-				<Navbar.Collapse>
+			<Container>
+				<Row>
 					<Nav>
-						<Nav.Link href="/">Home</Nav.Link>
-						<Nav.Link href="/about">About</Nav.Link>
+						<Nav.Item>
+							<Nav.Link href="/">Home</Nav.Link>
+						</Nav.Item>
+						<Nav.Item>
+							<Nav.Link href="/about">About</Nav.Link>
+						</Nav.Item>
 					</Nav>
-				</Navbar.Collapse>
-			</Navbar>
+				</Row>
+			</Container>
 		);
 	}
 }
